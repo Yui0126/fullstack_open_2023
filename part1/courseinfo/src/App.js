cee453d1c4cb4b3dc309bde9d6c734cb4890f224
Yui0126 <85067724+Yui0126@users.noreply.github.com>
2023-07-17 19:33:53 +0900

    removed

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,74 +1,26 @@
 import React from 'react'
-const Header = (props) => {
-    return ( <
-        h1 > { props.course.name } < /h1>
-    )
-}
 
-const Part = (props) => {
-    return ( <
-        div > { props.name } { props.exercises } < /div>
-    )
+import {useState} from 'react'
 
-}
+const Display = ({counter}) => <div>{counter}</div>
 
-const Content = (props) => {
-    return ( <
-        div >
-        <
-        Part name = { props.course.parts[0].name }
-        exercises = { props.course.parts[0].exercises }
-        / > <
-        Part name = { props.course.parts[1].name }
-        exercises = { props.course.parts[1].exercises }
-        / > <
-        Part name = { props.course.parts[2].name }
-        exercises = { props.course.parts[2].exercises }
-        / > < /
-        div >
-    )
-}
-
-const Total = (props) => {
-    return ( <
-        div > The total number of exercises is { props.total } < /div>
-    )
-}
+const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>
 
 const App = () => {
-    const course = {
-        name: 'Half Stack application development',
-        parts: [
-          {
-            name: 'Fundamentals of React',
-            exercises: 10
-          },
-          {
-            name: 'Using props to pass data',
-            exercises: 7
-          },
-          {
-            name: 'State of a component',
-            exercises: 14
-          }
-        ]
-      }
-    let myNums = []
-    for (let i = 0; i < course.parts.length; i++) {
-        myNums.push(course.parts[i].exercises)
-    }
-    const sum = myNums.reduce((partialSum, a) => partialSum + a, 0);
+    const [counter, setCounter] = useState(0)
+    const increaseByOne = () => setCounter(counter + 1)
+    const decreaseByOne = () => setCounter(counter - 1)
+    const setToZero = () => setCounter(0)
 
-    return ( <
-        div >
-        <
-        Header course = { course }
-        /> <Content course={course}/ > <
-        Total total = { sum }
-        / > < /
-        div >
+    return(
+        <div>
+        <Display counter={counter}/>
+        <Button handleClick={increaseByOne} text='plus'/>
+        <Button handleClick={setToZero} text='zero'/>
+        <Button handleClick={decreaseByOne} text='minus'/>
+        </div>
     )
-}
 
+}
 
 export default App;
