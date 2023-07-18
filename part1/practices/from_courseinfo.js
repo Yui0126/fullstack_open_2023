f8a4eca85f252c45a797ff0dc0f818e302ede0ad
Yui0126 <85067724+Yui0126@users.noreply.github.com>
2023-07-18 23:18:38 +0900

    add

--- part1/practices/from_courseinfo.js
+++ part1/practices/from_courseinfo.js
@@ -86,3 +86,75 @@
 //         </div>
 //     )
 // }
+
+const Display = ({counter}) => <div>{counter}</div>
+
+const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>
+
+
+const [counter, setCounter] = useState(0)
+const increaseByOne = () => setCounter(counter + 1)
+const decreaseByOne = () => setCounter(counter - 1)
+const setToZero = () => setCounter(0)
+
+return(
+    <div>
+    <Display counter={counter}/>
+    <Button handleClick={increaseByOne} text='plus'/>
+    <Button handleClick={setToZero} text='zero'/>
+    <Button handleClick={decreaseByOne} text='minus'/>
+    </div>
+)
+
+
+
+const History = (props) => {
+    if (props.allClicks.length === 0) {
+        return(
+            <div>
+                the app is used by pressing the buttons
+            </div>
+        )
+    }
+    return(
+        <div>button pressd history: {props.allClicks.join(' ')}</div>
+    )
+}
+
+const Button = ({handleClick, text}) => (
+    <button onClick={handleClick}>
+        {text}
+    </button>
+)
+
+const App = () => {
+
+
+    const [left, setLeft] = useState(0)
+    const [right, setRight] = useState(0)
+    const [allClicks, setAll] = useState([])
+
+    const hello = (who) => () => {
+        console.log('hello', who)
+    }
+    
+    const handleLeftClick = () => {
+        setAll(allClicks.concat('L'))
+        setLeft(left + 1)
+    }
+
+    const handleRightClick = () => {
+        setAll(allClicks.concat('R'))
+        setRight(right + 1)
+    }
+
+    return(
+        <div>
+            {left}
+            <Button handleClick={handleLeftClick} text='left'/>
+            <Button handleClick={handleRightClick} text='right'/>
+            {right}
+            <History allClicks={allClicks}/>
+        </div>
+    )
+}
